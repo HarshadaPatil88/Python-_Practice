9c20f35f59d7bfc8768ce55c110af0e86d867975
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:59:22 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,38 @@
+str = ("Apna College")
+ch = str[0]
+print(ch)
+
+ch = str[1]
+print(ch)
+
+ch = str[2]
+print(ch)
+
+ch = str[3]
+print(ch)
+
+print(str[4])
+
+print(str[5])
+
+print(str[6])
+
+print(str[7])
+
+print(str[8])
+
+print(str[9])
+
+print(str[10])
+
+print(str[11])
+
+
+
+
+
+
+
+
+
+
