2ea90112248682a0081aa88bdbc94f39e88b6e35
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:55:54 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,16 @@
+print("Hare Krishna")
+
+name = "Harshada"
+age = 22 
+price = 25.22
+old = False
+a = None
+
+print("My name is : ",name)
+print("My age is : ",age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
