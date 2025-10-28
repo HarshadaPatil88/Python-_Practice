4b4a11b50ebe04907b3b8bdd79530f4de808083b
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:58:10 +0530

    Add files via upload

---
+++ Input_Q.py
@@ -0,0 +1,30 @@
+# # Sum Of Two Numbers
+# num1 = int(input("Enter Num1 : "))
+# num2 = int(input("Enter Num2 : "))
+
+# print("Sum = ", num1+num2)
+
+
+# # Area of a Square
+# a = int(input("Enter side of a Square : "))
+
+# print("Area = ", a*a)
+
+# side = float(input("Enter square side : "))
+
+# print("Area = ", side ** 2)
+
+
+# # Average 
+# num1 = float(input("Enter Num1 : "))
+# num2 = float(input("Enter Num2 : "))
+
+# print("Avg = ", (num1+num2)/2)
+
+
+# # >=
+
+a = int(input("Enter Num1 : "))
+b = int(input("Enter Num2 : "))
+
+print(a>=b)
