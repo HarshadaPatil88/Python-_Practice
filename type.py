de758239b5d6a9151797ce153d69c0a5b1a7d203
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:57:08 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,35 @@
+#type conversion
+
+# a = 2
+# b = 4.25
+
+# sum = a + b  # 2.0 + 4.25 = 6.25
+# print(sum)
+
+
+#type casting
+
+a = int("2")
+b = 4.25
+
+print(type(a))
+sum = a + b  
+print(sum)
+
+a = 3.14
+a = str(a)
+print(type(a))
+
+######
+
+'''
+
+print(a+b)                    #error
+
+
+'''
+
+######
+
+
+
