20e2af19532c9fece857e54aeecd7ae378b04d54
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:56:39 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,87 @@
+# a = 1000
+# b = 500
+# sum = a + b
+# diff = a - b 
+# print(sum)
+# print(diff)
+
+## Arithmetic Operators 
+
+# a = 5
+# b = 2
+
+# print("Sum = ", a + b)
+# print("Diff = ", a - b)
+# print("Mul = ", a * b)
+# print("Divi = ", a / b)
+# print("Mod = ", a % b)  #Remainder
+# print("a^b = ", a ** b) #Power
+
+
+# #Relational Operators 
+
+# a = 20 
+# b = 50
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+
+
+# #Assignment Operators
+
+# num = 10
+# print("num = ",num)
+
+# num = 10
+# num += 5
+# print("num = ",num)
+
+# num = 10
+# num -= 5
+# print("num = ",num)
+
+# num = 10
+# num *= 5
+# print("num = ",num)
+
+# num = 10
+# num /= 5
+# print("num = ",num)
+
+# num = 10
+# num //= 5
+# print("num = ",num)
+
+# num = 10
+# num %= 5
+# print("num = ",num)
+
+# num = 10
+# num **= 5
+# print("num = ",num)
+
+
+#Logical Operators
+
+a = 50
+b = 20
+
+print(not False) #True
+print(not(a > b)) #False
+
+val1 = True
+val2 = False
+
+print("AND operator = ", val1 and val2) #False
+
+print("OR operator = ", val1 or val2) #True
+
+print("OR operator = ", (a==b) or (a>b)) #True
+
+print("AND operator = ", (a==b) and (a>b)) #False
+
+
