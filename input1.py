aab8600310388af7d643aed09894d9fcc8897023
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:57:40 +0530

    Add files via upload

---
+++ input1.py
@@ -0,0 +1,27 @@
+name = input("Enter Your Name : ")
+print("Welcome...",name)
+
+age = input("Enter Your Age : ")
+print("Age = ",age)
+
+value = input("Enter some value : ")
+print(type(value),value) #string
+
+value = int(input("Enter some value : "))
+print(type(value),value) #int
+
+value = float(input("Enter some value : "))
+print(type(value),value) #float
+
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+marks = float(input("Enter Your Marks: "))
+
+print("Welcome...",name)
+print("Age = ",age)
+print("Marks = ",marks)
+
+
+
+
+
