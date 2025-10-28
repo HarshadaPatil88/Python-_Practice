28ae802072ac5239098c98b1ea56006129a495fe
Harshada Patil <harshadap880@gmail.com>
2025-10-28 16:58:37 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,32 @@
+# # Different Ways:-
+# str1 = ("This is a string")
+# str2 = ('Apna COllege')
+# str3 = ("""Another String""")
+
+# # Escape Sequence Characters:
+# str1 = ("This is a string \n We are creating in python")
+# print(str1)
+
+# str1 = ("This is a string \nWe are creating in python")
+# print(str1)
+
+# str1 = ("This is a string \t We are creating in python")
+# print(str1)
+
+# Concatenation and Length of a String
+str1 = ("apna")
+print(len(str1))
+str2 = ("college")
+print(len(str2))
+str3 = (str1 + str2)
+print(len(str3))
+print(str3)
+
+str4 = (str1 + " " + str2)
+print(len(str4))
+print(str4)
+
+
+
+
+
