1d83fdcc944e9940aac405b9c9e7bf55b384126b
Harshada Patil <harshadap880@gmail.com>
2025-11-03 16:13:02 +0530

    Add files via upload

---
+++ string_Q.py
@@ -0,0 +1,37 @@
+## Q. on Length
+
+a = input("Write your first name : ")
+
+print("length of your name is : ", len(a))
+
+
+## Q. on Count
+
+str = ("I have $1000 money")
+print(str.count("$")) #1
+
+str = ("Hi $I am the $ symbol $99.9")
+print(str.count("$")) #3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
