6a07334ceceb99b3089dab739608e891e7a1678a
Harshada Patil <harshadap880@gmail.com>
2025-11-03 16:11:23 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,39 @@
+str = ("Hare Krishna")
+print(str[1:4])
+
+print(str[0:4])
+
+print(str[:4])
+
+print(str[0:len(str)])
+
+print(str[:])
+
+print(str[5:12])
+
+print(str[5:len(str)])
+
+print(str[5:])
+
+print(str[:4])  #[0:4]
+
+print(str[5:])  #[5:len(str)]
+
+print(str[2:2]) #blank space
+
+
+# # Negative Indexing
+
+str1 = ("Apple")
+print(str1[-4:-1])
+
+print(str1[-5:])
+
+print(str1[-5:len(str1)])
+
+print(str1[:-1])
+
+print(str1[-5:-2])
+
+
+
