6122c4def00f22dfb80b14c49164afbacef18805
Harshada Patil <harshadap880@gmail.com>
2025-11-04 14:59:25 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,35 @@
+# #Traffic Light
+light = "white"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+print("end of code")
+
+
+# vote
+age = int(input("Enter Your Age :"))
+
+if(age >= 18):
+    print("Can Vote")
+else:
+    print("Cannot Vote")
+
+
+
+
+
+
+
+
+
+
+
+
+
