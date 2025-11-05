415c6359e32e8fa58d8e45b230feb062f47368ba
Harshada Patil <harshadap880@gmail.com>
2025-11-05 16:10:13 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,30 @@
+# ## Even And Odd
+
+num = int (input("Enter a Number :"))
+
+rem = num % 2
+
+if(rem == 0):
+    print("Number is EVEN")
+else:
+    print("Number ids ODD")
+
+
+
+num = int (input("Enter a Number :"))
+
+if(num % 2 == 0):
+    print("Number is Even")
+else:
+    print("Number ids Odd")
+
+
+
+
+
+
+
+
+
+
+
