c480d05a87567804b652ef056b22efcb4d0e7f51
Harshada Patil <harshadap880@gmail.com>
2025-11-05 16:11:28 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,39 @@
+# ### Multiple of 7
+
+num = int(input("Enter Num : "))
+
+rem = num % 7
+
+if(rem == 0):
+    print("Num is multiple of 7")
+else:
+    print("Num is not a multiple of 7")
+
+
+
+num = int(input("Enter Num : "))
+
+if(num % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not a multiple of 7")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
